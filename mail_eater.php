4e71bcb8154ca84fdbe83e2e0d48cedc62d98c30
Doug McLean <dmclean@joimedia.com>
2013-11-28 13:45:03 -0700

    Records the header text now.

--- mail_eater.php
+++ mail_eater.php
@@ -12,9 +12,28 @@ $Parser->setStream(fopen("php://stdin","r"));
 $html = $Parser->getMessageBody('html');
 $text = $Parser->getMessageBody('text');
 
+
+$to = $Parser->getHeader('to');
+$delivered_to = $Parser->getHeader('delivered-to');
+$from = $Parser->getHeader('from');
+$subject = $Parser->getHeader('subject');
+$attachments = $Parser->getAttachments();
+
+// dump it so we can see
+$header .= "To: $to\n";
+$header .= "Delivered To: $delivered_to\n";
+$header .= "From: $from\n";
+$header .= "Subject: $subject\n";
+
+
+unlink($path . 'header.txt');
+$fh = fopen($path . 'header.txt','a');
+fwrite($fh, $header);
+
 unlink($path . 'html.html');
 $fh = fopen($path . 'html.html','a');
 fwrite($fh, $html);
+
 unlink($path . 'text.txt');
 $fh = fopen($path . 'text.txt','a');
 fwrite($fh, $text);
